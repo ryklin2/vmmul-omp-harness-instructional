ad7379d51bccfac21d20dec306224e47d751a7fa
ryklin2 <dmoguy@gmail.com>
2025-11-20 09:45:30 -0800

    Update benchmark.cpp

--- benchmark.cpp
+++ benchmark.cpp
@@ -93,12 +93,18 @@ int main(int argc, char** argv)
         memcpy((void *)Ycopy, (const void *)Y, sizeof(double)*n);
 
         // insert start timer code here
-
+        auto start_time = std::chrono::high_resolution_clock::now();
         // call the method to do the work
         my_dgemv(n, A, X, Y); 
 
         // insert end timer code here, and print out the elapsed time for this problem size
+        auto end_time = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed_seconds = end_time - start_time;
+        double elapsed_time = elapsed_seconds.count();
 
+        //oh yeah maybe we should print it
+        std::cout << "elapsed time: " << elasped_time << " seconds" << std::endl;
+        std::cout << "Performance: " << mflops << " MFLOPs" << std::endl;
 
         // now invoke the cblas method to compute the matrix-vector multiplye
         reference_dgemv(n, Acopy, Xcopy, Ycopy);
